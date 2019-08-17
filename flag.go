b7a494880640bb1156b9af801cce6c405f9be87b
Robert Liebowitz <rliebz@gmail.com>
2019-08-17 06:14:15 -0400

    Document FlagSet interface

--- flag.go
+++ flag.go
@@ -15,11 +15,24 @@ type Flag interface {
 
 // FlagSet is the interface for a set of flags.
 type FlagSet interface {
+	// Args returns the non-flag arguments passed.
 	Args() []string
+
+	// Changed returns whether a flag was explicitly passed to change its value.
 	Changed(name string) bool
+
+	// DefineBool creates a new boolean flag.
 	DefineBool(p *bool, name string, short string, value bool, usage string)
+
+	// DefineString creates a new string flag.
 	DefineString(p *string, name string, short string, value string, usage string)
+
+	// Has returns whether a flag exists by name.
 	Has(name string) bool
+
+	// HasShort returns whether a flag exists by short name.
 	HasShort(name string) bool
+
+	// Parse parses a set of command-line arguments.
 	Parse(args []string) error
 }
